94491d4e65253efdd5b21e9b6cc213355390ad22
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2023-10-09 12:36:18 +0200

    feat: info

--- components/informationslayout/Cardsinfo.jsx
+++ components/informationslayout/Cardsinfo.jsx
@@ -1,38 +1,52 @@
 import { Box, Grid, Image, Heading, Text } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import NextLink from 'next/link'
 
 const CardInfo = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-   
     axios
       .get("/api/getcat")
       .then((response) => setData(response.data.categories))
       .catch((error) => console.error(error));
   }, []);
 
- 
   if (!data) {
     return <div>Chargement...</div>;
   }
 
   return (
-    <Box mt={[20,40]} mb={[20,40]} id="premier-titre">
-      <Heading as="h2" size="2xl" textAlign="center" mb={20} color={"#fd0000"} >
+    <Box mt={[20, 40]} mb={[20, 40]} id="premier-titre">
+      <Heading as="h2" size="2xl" textAlign="center" mb={20} color={"#fd0000"}>
         CATEGORIES
       </Heading>
 
-      <Grid templateColumns={["repeat(1, 1fr)","repeat(3, 1fr)"]} gap={6} mx={[10,20]}>
+      <Grid
+        templateColumns={["repeat(1, 1fr)", "repeat(3, 1fr)"]}
+        gap={6}
+        mx={[10, 20]}
+      >
         {data.map((item, index) => (
-          <Box key={index} bg="#fd0000" p={8} borderRadius="md" boxShadow="md" color={"white"}>
-            <Image src={item.image} alt="Image" height={60} width={"2xl"}/>
-            <Heading as="h1" size="md" mt={8} fontSize={24}>
-              {item.name.toUpperCase()} (ETAPE PENTEST : {item.step})
-            </Heading>
-            <Text mt={2}>{item.description.toUpperCase()}</Text>
-          </Box>
+          <NextLink href={`/categorie/${item.name}`}>
+            <Box
+              key={index}
+              bg="#fd0000"
+              p={8}
+              borderRadius="md"
+              boxShadow="md"
+              color={"white"}
+              height={"xl"}
+              _hover={{bg: "blue.800"}}
+            >
+              <Image src={item.image} alt="Image" height={60} width={"2xl"} />
+              <Heading as="h1" size="md" mt={8} fontSize={24}>
+                {item.name.toUpperCase()} (ETAPE PENTEST : {item.step})
+              </Heading>
+              <Text mt={2}>{item.description.toUpperCase()}</Text>
+            </Box>
+          </NextLink>
         ))}
       </Grid>
     </Box>
---
+++ pages/categorie/[...slug].jsx
@@ -0,0 +1,32 @@
+import ContainerBlock from "@/components/ContainerBlock";
+import ScreenComponentUtils from "@/components/utilslayouts/ScreenImageUtils";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+export default function PageUtils() {
+    const router = useRouter()
+    const { slug } = router.query
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        axios
+      .get("/api/getcat")
+      .then((response) => {
+        response.data.categories.map((ele) => {
+            if(ele.name == slug) {
+                setData(ele)
+            }
+        })
+      })
+      .catch((error) => console.error(error));
+      console.log(data)
+    })
+
+    return (
+        <ContainerBlock>
+            <ScreenComponentUtils data={data}/>
+        </ContainerBlock>
+    )
+}
