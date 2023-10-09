051f5096ac31d2bc3ef8c821faaa6873c157608c
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2023-10-09 15:53:07 +0200

    feat: add utils

---
+++ pages/api/getutils.js
@@ -0,0 +1,11 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import linked from '../../datas/utils.json';
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    // Renvoyez les données JSON en tant que réponse
+    res.status(200).json(linked);
+  } else {
+    // Si la méthode HTTP utilisée n'est pas GET, renvoyez une erreur 405 (Method Not Allowed)
+    res.status(405).end();
+  }
+}
--- pages/categorie/[...slug].jsx
+++ pages/categorie/[...slug].jsx
@@ -3,12 +3,14 @@ import ScreenComponentUtils from "@/components/utilslayouts/ScreenImageUtils";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import CardInfoUtils from "@/components/utilslayouts/CardinfoUtils";
+import Footer from "@/components/Footer";
 
 export default function PageUtils() {
     const router = useRouter()
     const { slug } = router.query
     const [data, setData] = useState(null);
+    const [utilsdata, setUtilsdata] = useState([]); // Utilisez le useState pour gérer l'état
 
     useEffect(() => {
         axios
@@ -21,13 +23,22 @@ export default function PageUtils() {
         })
       })
       .catch((error) => console.error(error));
-      
-      
-    })
+
+        axios.get("/api/getutils")
+        .then((elem) => {
+            const filteredUtils = elem.data.utils.filter((res) => {
+                return res.categories.some((cat) => cat.categoriename === slug || cat.categoriename === slug[0]);
+            });
+            setUtilsdata(filteredUtils);
+        })
+        .catch((error) => console.error(error));
+    }, [slug]); // Assurez-vous d'inclure slug dans le tableau de dépendances
 
     return (
         <ContainerBlock>
             <ScreenComponentUtils data={data}/>
+            <CardInfoUtils datautils={utilsdata} />
+            <Footer/>
         </ContainerBlock>
     )
 }
